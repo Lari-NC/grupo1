2bbd41d1183e1df75e7b09f1b1c89c0ef26cee71
Candela <candelamarzaroli@gmail.com>
2023-11-16 20:06:45 -0300

    aa

---
+++ src/grupo1/buque/fases/Fase.java
@@ -0,0 +1,16 @@
+package grupo1.buque.fases;
+
+import grupo1.buque.Buque;
+
+public abstract class Fase {
+    
+    public Fase() {}
+    
+    
+    public abstract boolean condicionFase(Buque buque);
+    public abstract Fase siguiente();
+    
+    public boolean puedoCambiarFase(Buque buque) {
+    	return buque.getfase().siguiente().condicionFase(buque);
+    }
+}
---
+++ src/grupo1/filtrado/Buscador.java
@@ -0,0 +1,10 @@
+package grupo1.filtrado;
+
+import java.util.ArrayList;
+
+import grupo1.circuito.Circuito;
+
+public abstract class Buscador {
+	    public abstract ArrayList<Circuito> buscar(ArrayList<Circuito> lista);
+	}
+
