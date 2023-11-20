65613a60a8d2f3b4b07f05697ee0f80b86f9db62
Candela <candelamarzaroli@gmail.com>
2023-11-20 13:26:43 -0300

    empresaTransportista setters

--- src/grupo1/transporte/EmpresaTransportista.java
+++ src/grupo1/transporte/EmpresaTransportista.java
@@ -1,5 +1,18 @@
 package grupo1.transporte;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmpresaTransportista {
+	private List<Chofer> choferesRegistrados= new ArrayList<>();
+	private List<Camion> camionesRegistrados= new ArrayList<>();
 	
+	
+	public void addd(Chofer chofer) {
+		this.choferesRegistrados.add(chofer);
+	}
+	
+	public void addd(Camion camion) {
+		this.camionesRegistrados.add(camion);
+	}
 }
