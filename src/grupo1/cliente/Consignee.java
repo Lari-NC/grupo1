32c400f016c8598e102a3c06e470032d9f31a798
Lara Noelí Cardozo <cardozo.lara.edu@gmail.com>
2023-11-22 14:18:47 -0300

    Cambios Cliente

--- src/grupo1/cliente/Consignee.java
+++ src/grupo1/cliente/Consignee.java
@@ -2,8 +2,8 @@ package grupo1.cliente;
 
 public class Consignee extends Cliente {
     
-    public Consignee (String nombre, int dni, String domicilio, int telefono, String mail) {
-        super(nombre, dni, domicilio, telefono, mail);
+    public Consignee () {
+    	
     }
 
 	public void recibirMailParaRetiro() {
--- src/grupo1/cliente/Shipper.java
+++ src/grupo1/cliente/Shipper.java
@@ -4,8 +4,8 @@ import grupo1.Factura;
 
 public class Shipper extends Cliente {
     
-    public Shipper (String nombre, int dni, String domicilio, int telefono, String mail) {
-        super(nombre, dni, domicilio, telefono, mail);
+    public Shipper () {
+    	
     }
 
 	public void recibirFactura(Factura facturaOrden) {
