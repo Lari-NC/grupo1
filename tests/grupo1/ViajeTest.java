09ce6e2862998b085c9810d86901077aeda62ee8
Candela <candelamarzaroli@gmail.com>
2023-11-25 19:37:12 -0300

    test de factura que no va y probe algo de test viaje y no salioesta en blanco asi no hay conflictos

--- tests/grupo1/ViajeTest.java
+++ tests/grupo1/ViajeTest.java
@@ -1,19 +1,17 @@
 package grupo1;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
+
 class ViajeTest {
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+    @BeforeEach
+    public void setUp() {
+    }
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+    @Test
+    public void viajeSeCreaCorrectamente() {
+    }
 }
